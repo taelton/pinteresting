743594389b80ecf71633d754b1a4a3647a08cbaa
Tim Elton <tim.elton@gmx.com>
2015-02-18 23:16:47 +0000

    Add image upload with Paperclip

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -24,8 +24,11 @@ class PinsController < ApplicationController
 
   def create
     @pin = current_user.pins.build(pin_params)
-    @pin.save
-    respond_with(@pin)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
   def update
@@ -49,6 +52,6 @@ class PinsController < ApplicationController
   end
 
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
